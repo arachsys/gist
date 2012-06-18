9b9937292c1f66706468379573a872a5570e3bc7
Chris Webb <chris@arachsys.com>
2012-06-18 20:02:11 +0100

    Support multiple ID arguments to gist delete

--- gist.bash
+++ gist.bash
@@ -2,12 +2,12 @@ __gist_complete() {
   local CMDS='clone delete describe destroy list ls private public rm'
   if [ $COMP_CWORD -eq 1 ]; then
     COMPREPLY=($(compgen -W "$CMDS" -- "$2"))
-  elif [ $COMP_CWORD -eq 2 ]; then
-    case ${COMP_WORDS[1]} in
-      clone | delete | destroy | rm)
-        COMPREPLY=($(compgen -W "$(gist list | cut -d ' ' -f 1)" -- "$2"))
-        ;;
-    esac
+  elif [ $COMP_CWORD -eq 2 ] && [ "${COMP_WORDS[1]}" = "clone" ]; then
+    COMPREPLY=($(compgen -W "$(gist list | cut -d ' ' -f 1)" -- "$2"))
+  elif [ "${COMP_WORDS[1]}" = "delete" ] \
+         || [ "${COMP_WORDS[1]}" = "destroy" ] \
+         || [ "${COMP_WORDS[1]}" = "rm" ]; then
+    COMPREPLY=($(compgen -W "$(gist list | cut -d ' ' -f 1)" -- "$2"))
   fi
 }
 
